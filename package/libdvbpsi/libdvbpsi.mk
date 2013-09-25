873f781502e379a0cbdf9dab5ff89d96106a5f50
Julian Scheel <julian@jusst.de>
2013-09-25 17:59:08 +0200

    Add libdvbpsi package.
    libdvbpsi is a dependency of vlc to handle mpeg-ts streams.
    
    Signed-off-by: Julian Scheel <julian@jusst.de>

---
+++ package/libdvbpsi/libdvbpsi.mk
@@ -0,0 +1,14 @@
+#################################################################################
+#
+# libdvbpsi
+#
+#################################################################################
+LIBDVBPSI_VERSION = 0.2.2
+LIBDVBPSI_SOURCE = libdvbpsi-$(LIBDVBPSI_VERSION).tar.bz2
+LIBDVBPSI_SITE = http://download.videolan.org/pub/libdvbpsi/$(LIBDVBPSI_VERSION)
+LIBDVBPSI_LICENSE = GPLv2
+LIBDVBPSI_LICENSE_FILES = COPYING
+LIBDVBPSI_INSTALL_STAGING = YES
+LIBDVBPSI_INSTALL_TARGET = YES
+
+$(eval $(autotools-package))
