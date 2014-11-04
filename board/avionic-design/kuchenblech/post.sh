0f0c956256553e51fd9426e170babdb19d0caaec
Julian Scheel <julian@jusst.de>
2014-11-04 11:24:52 +0100

    Add avionic design Kuchenblech board scripts
    Board scripts to create a fit uImage containing kernel and device tree for the
    avionic design Kuchenblech.
    
    Signed-off-by: Julian Scheel <julian@jusst.de>

---
+++ board/avionic-design/kuchenblech/post.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+IMAGES="${1}"
+
+echo "Building uImage with FDT in ${IMAGES}"
+sed "s/IMAGES/$(echo $IMAGES | \
+	sed -e 's/\\/\\\\/g' -e 's/\//\\\//g' -e 's/&/\\\&/g')/g" \
+	board/avionic-design/kuchenblech/kernel_fdt.its.in > \
+	/tmp/kernel_fdt.its
+mkimage -f /tmp/kernel_fdt.its $IMAGES/uImage
